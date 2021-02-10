c0dfba11d533c5f40e8e327dbfead94ca90fae17
Linguistics <thylinguistics@gmail.com>
2021-02-10 12:20:51 -0800

    ...

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -72,4 +72,31 @@ tree.insert(15);
 tree.insert(35);
 tree.insert(4);
 tree.insert(12);
-console.log(tree);
+//console.log(tree);
+
+
+
+
+
+// solved using recursion
+function branchSums(root){
+  const sums = [];
+  calculateBranchSums(root, 0, sums);
+  return sums;
+}
+
+//helper
+function calculateBranchSums(node, runningSum, sums){
+    if(!node){
+        return;
+    }
+
+    const newRunningSum = runningSum + node.value;
+    if(!node.left && !node.right){
+        sums.push(newRunningSum);
+        return;
+    }
+
+    calculateBranchSums(node.left, newRunningSum, sums);
+    calculateBranchSums(node.right, newRunningSum, sums);
+}
