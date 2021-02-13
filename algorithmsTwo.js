83ba2834a1b418085014ae3c822bf2891510345f
James <Thylinguistics@gmail.com>
2021-02-13 11:30:27 -0800

    ...

--- algorithmsTwo.js
+++ algorithmsTwo.js
@@ -639,9 +639,19 @@ function reverseWordsInString(string) {
 
 //console.log(reverseWordsInString('testing abc 123'));
 
-function gitTest(){
-    return 0;
-}
 
+function plusOne(digits){
+    for(let i = digits.length-1; i >= 0; i --){
+        if(digits[i] !== 9){
+            digits[i] += 1;
+            return digits;
+        }else{
+            digits[i] = 0;
+        }
+    }
+    digits.unshift(1);
+    return digits;
+}
+console.log(plusOne([6,9,2,9]));
 
 
