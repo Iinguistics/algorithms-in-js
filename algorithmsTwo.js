910c51a9355ccc2c85ad159117a57b9c67fd8a23
James <Thylinguistics@gmail.com>
2021-04-11 15:13:19 -0700

    ...

--- algorithmsTwo.js
+++ algorithmsTwo.js
@@ -1154,24 +1154,28 @@ function nonConstructibleChange(coins) {
 
 
 
-// function sumZero(n){
 
-// }
-// console.log(sumZero(5));
+function mergeOverlappingIntervals(intervals){
+    const sortedIntervals = intervals.sort((a,b)=> a[0] - b[0]);
+    const mergedIntervals = [];
+    let currentInterval = sortedIntervals[0];
+    mergedIntervals.push(currentInterval);
+
+    for(const nextInterval of sortedIntervals){
+        const [_, currentIntervalEnd] = currentInterval;
+        const [ nextIntervalStart, nextIntervalEnd] = nextInterval;
+
+        if(currentIntervalEnd >= nextIntervalStart) currentInterval[1] = Math.max(currentIntervalEnd, nextIntervalEnd);
+        else{
+            currentInterval = nextInterval;
+            mergedIntervals.push(currentInterval);
+        }
+    }
+    return mergedIntervals;
+}
+
+//console.log(mergeOverlappingIntervals([[1,2], [3,5], [4,7], [6,8], [9,10]]));
+// [[1,2], [3,8], [9,10]]
 
 
 
-// function arrayPairSum(nums){
-
-
-
-// }
-//console.log(arrayPairSum(1,4,3,2));
-
-
-
-// function frequencySort(nums){
-    
-
-// }
-// console.log(frequencySort([1,1,2,2,2,3]));
