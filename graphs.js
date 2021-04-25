b9d7dc9cf40a5afec97d2da1793996824be430d3
James <Thylinguistics@gmail.com>
2021-04-24 23:23:49 -0700

    ...

--- graphs.js
+++ graphs.js
@@ -1,1 +1,26 @@
 // Graphs: connectivity, direction & cycles
+// vertices are the nodes/values & edges are the connections
+
+
+class Node{
+    constructor(name){
+    this.name = name;
+    this.children = [];
+    }
+
+    addChild(name){
+        this.children.push(new Node(name));
+        return this;
+    }
+
+  
+    // Time O(v + e) | space O(v)
+    depthFirstSearch(array){
+        array.push(this.name);
+        for(const child of this.children){
+            child.depthFirstSearch(array);
+        }
+        return array;
+    }
+}
+
