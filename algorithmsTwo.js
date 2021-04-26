500deced5e4c2b0491abd5ca2cf312151a29b334
James <Thylinguistics@gmail.com>
2021-04-25 18:56:10 -0700

    ...

--- algorithmsTwo.js
+++ algorithmsTwo.js
@@ -2004,3 +2004,99 @@ function maxNumberOfBalloons(text){
 
 
 
+function sherlockAndAnagrams(s){
+  const duplicatesCount = s.split('').filter((v, i) => s.indexOf(v) !== i).length;
+
+  if (!duplicatesCount) return 0;
+
+  let anagramsCount = 0;
+
+  const arr = getAllSubstrings(s)
+
+  for (let i = 0; i < arr.length; i++) {
+    anagramsCount += countAnagrams(i, arr)
+  }
+
+  return anagramsCount
+
+
+  // helpers
+  function getAllSubstrings(str) {
+    let i, j, result = [];
+  
+    for (i = 0; i < str.length; i++) {
+      for (j = i + 1; j < str.length + 1; j++) {
+        result.push(str.slice(i, j))
+      }
+    }
+    return result
+  }
+
+
+  function isAnagram(str1, str2) {
+    const hist = {}
+  
+    for (let i = 0; i < str1.length; i++) {
+      const char = str1[i]
+      if (hist[char]) {
+        hist[char]++
+      } else {
+        hist[char] = 1
+      }
+    }
+  
+    for (let j = 0; j < str2.length; j++) {
+      const char = str2[j]
+      if (hist[char]) {
+        hist[char]--
+      } else {
+        return false
+      }
+    }
+  
+    return true
+  }
+
+
+  function countAnagrams(currentIndex, arr) {
+    const currentElement = arr[currentIndex]
+    const arrRest = arr.slice(currentIndex + 1)
+    let counter = 0
+  
+    for (let i = 0; i < arrRest.length; i++) {
+      if (currentElement.length === arrRest[i].length && isAnagram(currentElement, arrRest[i])) {
+        counter++
+      }
+    }
+  
+   return counter
+  }
+
+}
+//console.log(sherlockAndAnagrams("abba"));
+
+
+
+// assuming its already sorted, return index, if target is not in the array return -1
+function binarySearch(array, target){
+    let left = 0;
+    let right = array.length - 1;
+    
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2);
+        if(array[mid] === target){
+            return mid;
+        }else if(array[mid] < target){
+            left = mid + 1;
+        }else{
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+
+}
+//console.log(binarySearch([0,1,21,33,45,45,61,71,72,73], 33));
+
+
+
